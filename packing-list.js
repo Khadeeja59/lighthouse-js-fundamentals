6cbb97064b82ad86ec9da9e698017db6a8df9d9f
Khadeeja59 <kkausar59@gmail.com>
2021-11-08 20:53:02 +0000

    packing list

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,16 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
+/* Using for loop too display elements.
 for (let i = 0; i < packingList.length; i++) {
  // console.log(packingList[i]);
 }
 console.log(packingList[3]);
+*/
+//Using do while loop
+
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
